a72505965a503d648b64598eae895f5f3f591da1
Ivan <ivan18drozdov@gmail.com>
2025-06-16 19:04:19 +0300

    fix: fixed user service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { VendorModule } from './vendor/vendor.module';
 import { BookModule } from './book/book.module';
 import { OrderModule } from './order/order.module';
 import { ReviewModule } from './review/review.module';
@@ -11,7 +10,7 @@ import { CategoryModule } from './category/category.module';
 import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [AuthModule, UserModule, VendorModule, BookModule, OrderModule, ReviewModule, CategoryModule, TagModule],
+  imports: [AuthModule, UserModule, BookModule, OrderModule, ReviewModule, CategoryModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
